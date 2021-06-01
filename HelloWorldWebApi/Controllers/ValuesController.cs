e6aea51c88c9259a74c89aa16fdbe3f5ccda52f1
elifnurelaldi <elif.elaldi@gmail.com>
2021-06-01 13:35:56 +0300

    Add project files.

---
+++ HelloWorldWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using HelloWorldWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<AppMessage> Get()
+        {
+            List<AppMessage> messages = new List<AppMessage>();
+            messages.Add(new AppMessage() { Message = "Hello World!" });
+            messages.Add(new AppMessage() { Message = "Hello Galaxy!" });
+            messages.Add(new AppMessage() { Message = "Hello Universe!" });
+            return messages;
+        }
+
+    }
+}
